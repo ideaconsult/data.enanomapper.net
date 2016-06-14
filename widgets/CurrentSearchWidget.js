7287e929bf4c06640f105b59e61a6dd1d71b57cd
theJonan <ivan@jonan.info>
2016-06-14 15:48:13 +0300

    - Current selection kind-of works.

--- widgets/CurrentSearchWidget.js
+++ widgets/CurrentSearchWidget.js
@@ -2,6 +2,7 @@
 
 AjaxSolr.CurrentSearchWidget = AjaxSolr.AbstractWidget.extend({
   start: 0,
+  fieldRegExp: /^[\w\.]+:/,
 
   afterRequest: function () {
     var self = this,
@@ -9,7 +10,7 @@ AjaxSolr.CurrentSearchWidget = AjaxSolr.AbstractWidget.extend({
 
     var q = this.manager.store.get('q').val();
     if (q != '*:*') {
-        links.push($('<a href="#"></a>').text(q).click(function () {
+        links.push(self.tagRenderer(q, "x", function () {
           self.manager.store.get('q').val('*:*');
           self.doRequest();
           return false;
@@ -18,20 +19,20 @@ AjaxSolr.CurrentSearchWidget = AjaxSolr.AbstractWidget.extend({
 
     var fq = this.manager.store.values('fq'),
         clearIdx = null;
+        
     for (var i = 0, l = fq.length; i < l; i++) {
-    	var style = 'class="tag_selected"';
-    	    
     	if (fq[i].indexOf("!collapse field=s_uuid") >= 0)
     	  clearIdx = i;
-      else
-    		links.push(self.tagRenderer(fq[i], null, self.removeFacet(fq[i])).addClass('tag_selected'));
+      else {
+    		links.push(self.tagRenderer(fq[i].replace(self.fieldRegExp, ""), "x", self.removeFacet(fq[i])).addClass('tag_selected'));
+      }
     }
     
     //have to ensure the collapsed query is not removed! 
     if (links.length > 1 && clearIdx !== null) {
-      links.unshift(self.tagRenderer("Clear filters", null, function () {
+      links.unshift(self.tagRenderer("Clear filters", "x", function () {
         self.manager.store.get('q').val('*:*');
-        self.manager.store.remove('fq');
+        self.manager.store.removeByValue('fq', self.fieldRegExp);
         self.doRequest();
         return false;
       }));
