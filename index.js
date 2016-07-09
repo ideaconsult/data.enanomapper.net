d306ced6968bdeeb57250ff5831c9dfe48f52b32
theJonan <ivan@jonan.info>
2016-07-09 20:31:21 +0300

    - In-widget filtering implemented.

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ $(document).ready(function() {
 	$( "#smartmenu" ).smartmenus();    
 	$("#search").find('input').autocomplete();
 		
+	// Prepare the widget filter UI prepartion method.
 	var initWidgetFilter = function( event, ui ) {
 		if (!!ui.oldHeader && !!ui.oldHeader[0])
 			$("span.ui-icon-search", ui.oldHeader[0]).remove();
 
 		if (!!ui.newPanel && !!ui.newPanel[0]) {
 			if (ui.newPanel[0].scrollHeight > ui.newPanel[0].clientHeight) {
-	  		$("input.widget-filter", ui.newPanel[0]).show();	
+	  		$("input.widget-filter", ui.newPanel[0]).show().val("");
 		  			
   			var fe = $("<span class=\"ui-icon ui-icon-search\"></span>").on("click", function (e) {
 					ui.newPanel.animate({ scrollTop: ui.newPanel.scrollTop() > 0 ? 0 : widgetFilterScroll });
+					$("input.widget-filter", ui.newPanel[0]).focus();
 					e.stopPropagation();
 					e.preventDefault();
   			});
@@ -29,6 +31,7 @@ $(document).ready(function() {
   	}
 	};
 	
+	// Now instantiate the accordion...
 	$( "#accordion" ).accordion({
 		heightStyle: "content",
 		collapsible: true,
@@ -37,9 +40,26 @@ $(document).ready(function() {
 		activate: initWidgetFilter
 	});
 	
+	// .. assign the widget-initialization funtion for each panel ...
 	$("#accordion div.widget-content").each(function () {
 		var jq = $(this);
 		jq.data('initWidget', function(e) { initWidgetFilter( e, { newPanel: jq, newHeader: jq.prev() } )});
+	});
+
+	// ... and prepare the actual filtering funtion.
+	$("#accordion input.widget-filter").on("keyup", function (e) {
+		var needle = $(this).val().toLowerCase(),
+				div = $(this).parent('div.widget-content');
+
+		if (needle == "")
+			$('li', div[0]).show();
+		else
+			$('li>a', div[0]).each( function () {
+				if (this.title.toLowerCase().indexOf(needle) >= 0 || this.innerText.toLowerCase().indexOf(needle) >= 0)
+					$(this).parent().show();
+				else
+					$(this).parent().hide();
+			});
 	});
 	
 	$( "#result-tabs" ).tabs();
