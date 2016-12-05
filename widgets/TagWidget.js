a7b01eea6c2c704bd3dfe513735694bdffc3e93f
theJonan <ivan@jonan.info>
2016-12-05 15:00:54 +0200

    - Reflecting the small changes in SolrJsX

--- widgets/TagWidget.js
+++ widgets/TagWidget.js
@@ -36,7 +36,7 @@ jT.TagWidgeting.prototype = {
     this.target.empty();
     for (var i = 0, l = objectedItems.length; i < l; i++) {
       facet = objectedItems[i].facet;
-      selected = this.facetSelected(facet);
+      selected = this.hasValue(facet);
       
       this.target.append(el = this.renderTag(facet, objectedItems[i].count, selected ? nullf : this.clickHandler(facet)));
       
@@ -47,19 +47,6 @@ jT.TagWidgeting.prototype = {
     hdr.textContent = jT.ui.updateCounter(hdr.textContent, total);
     if (!!refresh)
     	refresh.call();
-  },
-  
-  facetSelected: function (value) {
-    var indices = this.manager.findParameters('fq', this.fieldRegExp),
-        value = Solr.escapeValue(value);
-        
-    for (var p, i = 0, il = indices.length; i < il; ++i) {
-      p = this.manager.getParameter('fq', indices[i]);
-      if (p.value.replace(this.fieldRegExp, "").indexOf(value) > -1)
-        return true;
-    }
-    
-    return false;
   }
 };
 
